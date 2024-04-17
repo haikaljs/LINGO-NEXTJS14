e4f5880482ea8a87d53df32787963e05fbf44862
haikaljs <haikaljavascript@gmail.com>
2024-04-18 02:31:02 +0800

    buttons library

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
-      <p className="text-green-500 font-bold">Hello Lingo!</p>
+      <Button>Default</Button>
+      <Button>Primary</Button>
+      <Button>Default</Button>
     </div>
   );
 }
