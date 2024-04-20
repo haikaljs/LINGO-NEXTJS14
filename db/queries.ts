fdcbeca6b8610ecaf634ab23b53562a959ce6bb2
haikaljs <haikaljavascript@gmail.com>
2024-04-20 16:01:33 +0800

    lesson button

--- db/queries.ts
+++ db/queries.ts
@@ -2,7 +2,14 @@ import { cache } from "react";
 import db from "@/db/drizzle";
 import { auth } from "@clerk/nextjs";
 import { eq } from "drizzle-orm";
-import { challenges, courses, lessons, units, userProgress } from "./schema";
+import {
+  challenges,
+  courses,
+  lessons,
+  units,
+  userProgress,
+  challengeProgress,
+} from "./schema";
 
 export const getUserProgress = cache(async () => {
   const { userId } = await auth();
@@ -22,12 +29,14 @@ export const getUserProgress = cache(async () => {
 });
 
 export const getUnits = cache(async () => {
+  const { userId } = await auth();
   const userProgress = await getUserProgress();
 
-  if (!userProgress?.activeCourseId) {
+  if (!userId || !userProgress?.activeCourseId) {
     return [];
   }
 
+  // TODO: Confirm wether order in needed
   const data = await db.query.units.findMany({
     where: eq(units.courseId, userProgress.activeCourseId),
     with: {
@@ -35,7 +44,9 @@ export const getUnits = cache(async () => {
         with: {
           challenges: {
             with: {
-              challengeProgress: true,
+              challengeProgress: {
+                where: eq(challengeProgress.userId, userId),
+              },
             },
           },
         },
